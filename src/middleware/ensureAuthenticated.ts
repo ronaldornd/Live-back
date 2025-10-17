d5566b297c934b73da37b5968e7621d45e13945b
Ronaldo Barbosa <ronaldo.s.barbosa@outlook.com>
2025-10-17 20:19:31 -0300

    update

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -1,28 +1,27 @@
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { verify } from "jsonwebtoken";
 
-interface IPayLoad{
-    sub:string
+interface IPayLoad {
+    sub: string
 }
 
-export function ensureAutenticated(request: Request, response: Response, next: NextFunction){
+export function ensureAutenticated(request: Request, response: Response, next: NextFunction) {
     const authToken = request.headers.authorization;
 
     if (!authToken) {
-            return response.status(401).json({
-                errorCode: "token_invalid"
-            });
+        return response.status(401).json({
+            errorCode: "token_invalid"
+        });
     }
     const [, token] = authToken.split(" ");
-    
-    try {
 
-        const { sub } = verify(token, process.env.JWT_SECRET) as IPayLoad;
-        
+    try {
+        const { sub } = verify(token, process.env.JWT_SECRET as string) as IPayLoad;
+
         request.user_id = sub;
         return next();
     } catch (error) {
-        return response.status(401).json({ errorCode: error})
+        return response.status(401).json({ errorCode: error })
     }
 
 }
--- src/services/AuthenticateUserServiceApp.ts
+++ src/services/AuthenticateUserServiceApp.ts
@@ -1,24 +1,24 @@
 import axios from "axios";
 import "dotenv/config";
 import prismaClient from "../prisma";
-import {sign} from "jsonwebtoken";
-interface IaccesTokenResponse{
+import { sign } from "jsonwebtoken";
+interface IaccesTokenResponse {
     access_token: string;
 }
 
-interface IUserResponse{
+interface IUserResponse {
     avatar_url: string,
-    login:string,
+    login: string,
     id: number,
-    name:string
+    name: string
 }
 
 
 class AuthenticateUserServiceApp {
-    async execute(code:string){
+    async execute(code: string) {
         const url = "https://github.com/login/oauth/access_token";
 
-        const {data: accessTokenResponse} = await axios.post<IaccesTokenResponse>( url , null ,{
+        const { data: accessTokenResponse } = await axios.post<IaccesTokenResponse>(url, null, {
             params: {
                 client_id: process.env.CLIENT_ID_NATIVE,
                 client_secret: process.env.CLIENT_SECRET_NATIVE,
@@ -27,22 +27,22 @@ class AuthenticateUserServiceApp {
             headers: {
                 "accept": "application/json"
             }
-        
+
         })
-        console.log({data: accessTokenResponse});
-        const response = await axios.get<IUserResponse>("https://api.github.com/user",{
-            headers:{
-                authorization:`Bearer ${accessTokenResponse.access_token}`
+        console.log({ data: accessTokenResponse });
+        const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
             }
         })
 
-        const {login,id, avatar_url,name} = response.data;
-        let user = await prismaClient.user.findFirst({where:{github_id: id}});
+        const { login, id, avatar_url, name } = response.data;
+        let user = await prismaClient.user.findFirst({ where: { github_id: id } });
 
-        if(!user){
-            await  prismaClient.user.create({
-                data:{
-                    github_id:id,
+        if (!user) {
+            user = await prismaClient.user.create({
+                data: {
+                    github_id: id,
                     login: login,
                     avatar_url: avatar_url,
                     name: name
@@ -51,20 +51,21 @@ class AuthenticateUserServiceApp {
         }
 
         const token = sign({
-            user:{
-                name:user.name,
-                avatar_url:user.avatar_url,
-                id:user.id
+            user: {
+                name: user.name,
+                avatar_url: user.avatar_url,
+                id: user.id
             }
-            
+
         },
-        process.env.JWT_SECRET,
-        {
-            subject:user.id,
-            expiresIn:"1d"
-        })
-        return {token,user};
+            process.env.JWT_SECRET as string,
+            {
+                subject: user.id,
+                expiresIn: "1d",
+                algorithm: "HS256"
+            })
+        return { token, user };
     }
 }
 
-export {AuthenticateUserServiceApp}
+export { AuthenticateUserServiceApp }
